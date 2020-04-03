38b630acb5a91052aab86270fe894c88d401179c
Glitch (hello-express) <none>
2020-04-03 11:33:10 +0000

    🍐🗼 Checkpoint ./commands/moderation/kick.js:5895069/814

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -7,6 +7,26 @@ module.exports = {
   usage: "kick <@user> <raeson>",
   run: (client, message, args) => {
     
+    if(!message.member.hasPermission("KICK_MEMBERS")) {
+      return message.channel.send(`**${message.author.username}**, You do not have enough permission to use this command`)
+    }
+    
+    if(!message.guild.me.hasPermission("KICK_MEMBERS")) {
+      return message.channel.send(`**${message.author.username}**, I do not have enough permission to use this command`)
+    }
+    
+    let target = message.mentions.members.first();
+    
+    if(!target) {
+      return message.channel.send(`**${message.author.username}**, Please mention the person who you want to kick`)
+    }
+    
+    if(target.id === message.author.id) {
+     return message.channel.send(`**${message.author.username}**, You can not kick yourself`)
+    }
+    
+
+    
     
   }
 }
