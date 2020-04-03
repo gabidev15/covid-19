6b1dcc1402b3d0c3167156f6867bb78878070ca6
Glitch (hello-express) <none>
2020-04-03 11:21:41 +0000

    📹🐞 Checkpoint ./commands/moderation/kick.js:5895069/261

---
+++ commands/moderation/kick.js
@@ -0,0 +1,12 @@
+const discord = require("discord.js");
+
+module.exports = {
+  name: "kick",
+  category: "moderation",
+  description: "Kick anyone with one shot xD",
+  usage: "kick <@user> <raeson>",
+  run: (client, message, args) => {
+    
+    
+  }
+}
