c7e197f8ecdbe20e5955aab84e6adefd18e00503
Glitch (hello-express) <none>
2020-04-03 09:48:15 +0000

    🐩🚩 Checkpoint ./package.json:5895069/4176 ./server.js:5895069/341 ./handlers/command.js:5895069/1306

--- server.js
+++ server.js
@@ -8,15 +8,26 @@ const client = new discord.Client({
 client.commands = new discord.Collection();
 client.aliases = new discord.Collection();
 
+
+
+
+["command"].forEach(handler => { //some error here
+  require(`./handlers/${handler}`)(client)
+})
+
+
+
 client.on("ready", () => { //When bot is ready
   console.log("I am Reday to Go")
   client.user.setActivity("I am Devil") //It will set status :)
 })
 
 client.on("message", message => {
- if(message.content === `${prefix}ping`) {
-   return message.channel.send(`Pong ${client.ws.ping}`)
- }
-})
+  
+if(message.author.bot) return;
+  if(!message.guild) return;
+  if(!message.content.startsWith(prefix)) return;
+  
+ })
 
 client.login(token)
