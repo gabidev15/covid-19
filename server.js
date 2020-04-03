e0828b1d458882f8e9b05390f0bdef032f8abed9
Glitch (hello-express) <none>
2020-04-03 09:36:32 +0000

    🚿🎅 Checkpoint ./server.js:5895069/186

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const { token, prefix } = require("./config.json")
-
+const { config } = require("dotenv");
 const discord = require("discord.js") //Gonna use Discord.js Module xD
-const client = new discord.Client();
+const client = new discord.Client({
+  disableEveryone: true
+});
+
+client.commands = new discord.Collection();
+client.aliases = new discord.Collection();
 
 client.on("ready", () => { //When bot is ready
   console.log("I am Reday to Go")
