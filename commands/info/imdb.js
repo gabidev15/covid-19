2f26b1ec4774c678197ac91499e6d78e0c4a8847
Glitch (hello-express) <none>
2020-04-17 14:43:16 +0000

    🗃👵 Checkpoint ./commands/info/imdb.js:5895069/467 ./config.json:5895069/118

--- commands/info/imdb.js
+++ commands/info/imdb.js
@@ -8,6 +8,10 @@ name: "imdb",
   usage: "imdb <name>",
   run: async (client, message, args, color) => {
     
+    if(!args.length) {
+      return message.channel.send("Please give the name of movie or series")
+    }
+    
     const imob = new imdb.Client({apiKey: "5e36f0db"}) //You need to paste you imdb api
     
     let movie = await imob.get({'name': args.join(" ")})
@@ -16,7 +20,15 @@ name: "imdb",
     .setTitle(movie.title)
     .setColor("#ff2050")
     .setThumbnail(movie.poster)
-    .set
+    .setDescription(movie.plot)
+    .setFooter(`Ratings: ${movie.rating}`)
+    .addField("Country", movie.country, true)
+    .addField("Languages", movie.languages, true)
+    .addField("Type", movie.type, true);
+    
+    
+    message.channel.send(embed)
+    
     
     
   }
