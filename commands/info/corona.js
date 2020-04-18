d2c8bdca32e2069bd2fb94434c1a889c295d69ee
Glitch (hello-express) <none>
2020-04-18 15:46:33 +0000

    💂🦁 Checkpoint ./commands/info/corona.js:5895069/1147

--- commands/info/corona.js
+++ commands/info/corona.js
@@ -21,8 +21,32 @@ module.exports = {
       .setTitle("Global Cases")
       .setColor("#ff2050")
       .setDescription("Sometimes cases number may differ from small amount.")
-      .addField("")
+      .addField("Total Cases", corona.cases, true)
+      .addField("Total Deaths", corona.deaths, true)
+      .addField("Total Recovered", corona.recovered, true)
+      .addField("Today's Cases", corona.todayCases, true)
+      .addField("Today's Deaths", corona.todayDeaths, true)
+      .addField("Active Cases", corona.active, true);
       
+      return message.channel.send(embed)
+      
+      
+      
+    } else {
+      let corona = await track.countries(args.join(" ")) //change it to countries
+      
+      let embed = new discord.MessageEmbed()
+      .setTitle(`${corona.country}`)
+      .setColor("#ff2050")
+      .setDescription("Sometimes cases number may differ from small amount.")
+      .addField("Total Cases", corona.cases, true)
+      .addField("Total Deaths", corona.deaths, true)
+      .addField("Total Recovered", corona.recovered, true)
+      .addField("Today's Cases", corona.todayCases, true)
+      .addField("Today's Deaths", corona.todayDeaths, true)
+      .addField("Active Cases", corona.active, true);
+      
+      return message.channel.send(embed)
       
       
     }
