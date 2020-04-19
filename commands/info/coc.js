304ce611da2a7b5db73de48a4e209f85f3cbb29e
Glitch (hello-express) <none>
2020-04-19 09:09:38 +0000

    🍕🍧 Checkpoint ./package.json:5895069/1606 ./commands/info/coc.js:5895069/3360

--- commands/info/coc.js
+++ commands/info/coc.js
@@ -1,8 +1,8 @@
 const clashApi = require('clash-of-clans-api');
-let client = clashApi({
+
+let clienx = clashApi({
   token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImE0ZGFmMjc0LTRlZWMtNDkwMC04NGFlLTEwYmE0N2VjMmRhYSIsImlhdCI6MTU4NzI4NTY2OCwic3ViIjoiZGV2ZWxvcGVyL2I2OWMyMWVkLTM2ZDQtZDg0Mi1kMDMxLTc5MjNlZDI1Njc2YSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjMuMjIwLjE2OS4xNTYiXSwidHlwZSI6ImNsaWVudCJ9XX0.WanAmI2ToRO2OurByvR4uJUyyvJLfxJeBX3PM52p3d1ckc9KlVd1Y4KKR7LhRogRxxjCNT-AGgMaekvo5UrJvg" 
 });
-
 
 
 
@@ -11,8 +11,16 @@ module.exports = {
   category: "info",
   description: "Get bot ping :/",
   usage: "ping",
-  run: (client, message) => {
-    message.channel.send(`**Pong** ${client.ws.ping}`);
+  run: async (client, message, args) => {
+    message.channel.send(`Done`);
+    
+   clienx
+  .clans()
+  .withWarFrequency('always')
+  .withMinMembers(25)
+  .fetch()
+  .then(response => console.log(response))
+  .catch(err => console.log(err))
   }
   
 }
